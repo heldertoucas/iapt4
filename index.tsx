ac74e7defbc40014c901adcd84e525d68df7855c
heldertoucas <heldertoucas@gmail.com>
2025-07-08 22:39:00 +0100

    Add global typography scale

--- index.tsx
+++ index.tsx
@@ -6,11 +6,13 @@ import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import './index.css';
+import { appConfig } from './src/config/appConfig';
 
 
 // --- Mount Application to the DOM ---
 const rootElement = document.getElementById('root');
 if (rootElement) {
+    document.documentElement.style.setProperty('--font-scale', String(appConfig.fontScale));
     const root = ReactDOM.createRoot(rootElement);
     root.render(
       <StrictMode>
